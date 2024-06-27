d12c4af741d9b8be28976b06748d52a58f0874f3
Ruben van der Linde <ruben@conduction.nl>
2024-06-27 09:23:00 +0200

    Typo fix

--- src/catalogiScript.js
+++ src/catalogiScript.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import catalogiaIndex from "./views/catalogi/index.vue";
+import catalogiIndex from "./views/catalogi/index.vue";
 Vue.mixin({ methods: { t, n } });
 
 const View = Vue.extend(catalogiIndex);
