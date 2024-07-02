ca186fec75f0b41092bd8ac15d4ca9fd449a25de
Ruben van der Linde <ruben@conduction.nl>
2024-07-02 07:51:55 +0200

    typo fix

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 import { NcContent } from '@nextcloud/vue'
 import MainMenu from './navigation/MainMenu.vue'
-import Modals from './modals/Modals.vue'
+import Modals from './modals/modals.vue'
 import Views from './views/views.vue'
 import { store } from './store.js'
 
