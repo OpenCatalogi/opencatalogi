7e03e287a9ac8cc3979a25338f856883638894fc
Ruben van der Linde <rubenvdlinde@gmail.com>
2024-07-15 14:19:29 +0200

    Added a sidebar to search

---
+++ src/sidebars/SideBars.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { store } from '../store.js'
+</script>
+
+<template>
+	<!-- Placeholder div for all of the vieuws-->
+	<SearchSideBar v-if="store.selected === 'search'" />
+</template>
+
+<script>
+import SearchSideBar from './search/SearchSideBar.vue'
+
+export default {
+	name: 'SideBars',
+	components: {
+		SearchSideBar,
+	},
+}
+</script>
---
+++ src/sidebars/search/SearchSideBar.vue
@@ -0,0 +1,49 @@
+<template>
+	<NcAppSidebar
+		:starred="starred"
+		name="cat-picture.jpg"
+		subname="last edited 3 weeks ago">
+		<NcAppSidebarTab name="Search" id="search-tab">
+			<template #icon>
+				<Magnify :size="20" />
+			</template>
+			Search tab content
+		</NcAppSidebarTab>
+		<NcAppSidebarTab name="Settings" id="settings-tab">
+			<template #icon>
+				<Cog :size="20" />
+			</template>
+			Settings tab content
+		</NcAppSidebarTab>
+		<NcAppSidebarTab name="Sharing" id="share-tab">
+			<template #icon>
+				<ShareVariant :size="20" />
+			</template>
+			Sharing tab content
+		</NcAppSidebarTab>
+	</NcAppSidebar>
+</template>
+<script>
+
+import { NcAppSidebar, NcAppSidebarTab } from '@nextcloud/vue'
+import Magnify from 'vue-material-design-icons/Magnify.vue'
+import Cog from 'vue-material-design-icons/Cog.vue'
+import ShareVariant from 'vue-material-design-icons/ShareVariant.vue'
+
+export default {
+	name: 'SearchSideBar',
+	components: {
+		NcAppSidebar,
+		NcAppSidebarTab,
+		// Icons
+		Magnify,
+		Cog,
+		ShareVariant,
+	},
+	data() {
+		return {
+			starred: false,
+		}
+	},
+}
+</script>
