e62ead12768a619ff8e730f24028852b9105d4ea
Thijn Douwma <thijn@conduction.nl>
2024-07-26 10:00:31 +0200

    fixed publication test

--- src/store/modules/publication.spec.js
+++ src/store/modules/publication.spec.js
@@ -2,6 +2,7 @@
 import { setActivePinia, createPinia } from 'pinia'
 
 import { usePublicationStore } from './publication.js'
+import { Attachment, Publication } from '../../entities/index.js'
 
 describe('Metadata Store', () => {
 	beforeEach(() => {
@@ -13,7 +14,21 @@ describe('Metadata Store', () => {
 
 		store.setPublicationItem(testData[0])
 
+		expect(store.publicationItem).toBeInstanceOf(Publication)
 		expect(store.publicationItem).toEqual(testData[0])
+		expect(store.publicationItem.validate()).toBe(true)
+
+		store.setPublicationItem(testData[1])
+
+		expect(store.publicationItem).toBeInstanceOf(Publication)
+		expect(store.publicationItem).not.toEqual(testData[1])
+		expect(store.publicationItem.validate()).toBe(true)
+
+		store.setPublicationItem(testData[2])
+
+		expect(store.publicationItem).toBeInstanceOf(Publication)
+		expect(store.publicationItem).toEqual(testData[2])
+		expect(store.publicationItem.validate()).toBe(false)
 	})
 
 	it('sets publication list correctly', () => {
@@ -21,48 +36,50 @@ describe('Metadata Store', () => {
 
 		store.setPublicationList(testData)
 
-		expect(store.publicationList).toEqual(testData)
+		expect(store.publicationList).toHaveLength(testData.length)
+
+		expect(store.publicationList[0]).toBeInstanceOf(Publication)
+		expect(store.publicationList[0]).toEqual(testData[0])
+		expect(store.publicationList[0].validate()).toBe(true)
+
+		expect(store.publicationList[1]).toBeInstanceOf(Publication)
+		expect(store.publicationList[1]).not.toEqual(testData[1])
+		expect(store.publicationList[1].validate()).toBe(true)
+
+		expect(store.publicationList[2]).toBeInstanceOf(Publication)
+		expect(store.publicationList[2]).toEqual(testData[2])
+		expect(store.publicationList[2].validate()).toBe(false)
 	})
 
 	// TODO: fix this
 	it('set publication data.data property key correctly', () => {
 		const store = usePublicationStore()
 
-		store.setPublicationItem(testData[0])
 		store.setPublicationDataKey('contactPoint')
 
-		expect(store.publicationItem).toEqual(testData[0])
 		expect(store.publicationDataKey).toBe('contactPoint')
 	})
 
-	// TODO: fix this
 	it('set attachment item correctly', () => {
 		const store = usePublicationStore()
 
-		store.setAttachmentItem(testData[0].attachments[0])
+		store.setAttachmentItem(attachmentTestData[0])
 
-		expect(store.attachmentItem.id).toBe(testData[0].attachments[0].id)
-		expect(store.attachmentItem.reference).toBe('') // some of these are '' because its not in the test data
-		expect(store.attachmentItem.title).toBe(testData[0].attachments[0].title)
-		expect(store.attachmentItem.summary).toBe('')
-		expect(store.attachmentItem.description).toBe(testData[0].attachments[0].description)
-		expect(store.attachmentItem.labels).toEqual([])
-		expect(store.attachmentItem.accessURL).toBe(testData[0].attachments[0].accessURL)
-		expect(store.attachmentItem.downloadURL).toBe(testData[0].attachments[0].downloadURL)
-		expect(store.attachmentItem.type).toBe(testData[0].attachments[0].type)
-		expect(store.attachmentItem.extension).toBe('')
-		expect(store.attachmentItem.size).toBe(0)
-		// attachmentItem.anonymization
-		expect(store.attachmentItem.anonymization.anonymized).toBe(false)
-		expect(store.attachmentItem.anonymization.results).toBe('')
-		// attachmentItem.language
-		expect(store.attachmentItem.language.code).toBe('')
-		expect(store.attachmentItem.language.level).toBe('')
-		expect(store.attachmentItem.version_of).toBe(false)
-		expect(store.attachmentItem.hash).toBe(false)
-		expect(store.attachmentItem.published).toBe(testData[0].attachments[0].published)
-		expect(store.attachmentItem.modified).toBe(testData[0].attachments[0].modified)
-		expect(store.attachmentItem.license).toBe(testData[0].attachments[0].license)
+		expect(store.attachmentItem).toBeInstanceOf(Attachment)
+		expect(store.attachmentItem).toEqual(attachmentTestData[0])
+		expect(store.attachmentItem.validate()).toBe(true)
+
+		store.setAttachmentItem(attachmentTestData[1])
+
+		expect(store.attachmentItem).toBeInstanceOf(Attachment)
+		expect(store.attachmentItem).not.toEqual(attachmentTestData[1])
+		expect(store.attachmentItem.validate()).toBe(true)
+
+		store.setAttachmentItem(attachmentTestData[2])
+
+		expect(store.attachmentItem).toBeInstanceOf(Attachment)
+		expect(store.attachmentItem).toEqual(attachmentTestData[2])
+		expect(store.attachmentItem.validate()).toBe(false)
 	})
 })
 
@@ -139,5 +156,65 @@ const testData = [
 		themes: 'theme1',
 		anonymization: { anonymized: 'yes', results: 'success' },
 		language: { code: 'en', level: 'native' },
+	},
+]
+
+const attachmentTestData = [
+	{ // full data
+		id: '9044ab1e-cf5a-490a-be74-6be7a0c48a5f',
+		reference: 'ref1',
+		title: 'test 1',
+		summary: 'a short form summary',
+		description: 'a really really long description about this catalogus',
+		labels: [{ tag: 'tag1' }],
+		accessURL: 'https://example.com/access',
+		downloadURL: 'https://example.com/download',
+		type: 'document',
+		extension: 'pdf',
+		size: 1024,
+		anonymization: { anonymized: 'yes', results: 'success' },
+		language: { code: 'en', level: 'native' },
+		versionOf: 'v1.0',
+		hash: 'abc123',
+		published: '2024-01-01',
+		modified: '2024-01-02',
+		license: 'MIT',
+	},
+	{ // partial data
+		id: 'f849f287-492d-4100-91e1-1c4137f0abb5',
+		reference: 'ref2',
+		title: 'test 2',
+		summary: 'a short form summary',
+		description: 'a really really long description about this catalogus',
+		labels: [{ tag: 'tag2' }],
+		accessURL: 'https://example.com/access',
+		downloadURL: 'https://example.com/download',
+		type: 'document',
+		extension: 'pdf',
+		size: 1024,
+		anonymization: { anonymized: 'yes', results: 'success' },
+		language: { code: 'en', level: 'native' },
+		versionOf: 'v1.0',
+		license: 'MIT',
+	},
+	{ // invalid data
+		id: 'e193ea6b-1222-44cf-a71c-6ddcc232a79b',
+		reference: 'ref3',
+		title: '',
+		summary: 'a short form summary',
+		description: 'a really really long description about this catalogus',
+		labels: [{ tag: 'tag3' }],
+		accessURL: 'https://example.com/access',
+		downloadURL: 'https://example.com/download',
+		type: 'document',
+		extension: 'pdf',
+		size: 1024,
+		anonymization: { anonymized: 'yes', results: 'success' },
+		language: { code: 'en', level: 'native' },
+		versionOf: 'v1.0',
+		hash: 'abc123',
+		published: '2024-01-01',
+		modified: '2024-01-02',
+		license: 'MIT',
 	},
 ]
