424e29662a0c1d73f8db8782f8ab2e8da43fb76d
Mwest2020 <mwesterweel@hotmail.com>
2024-08-05 14:59:32 +0200

    OC app visible

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-// require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 \OC_App::loadApp(OCA\OpenCatalogi\AppInfo\Application::APP_ID);
 OC_Hook::clear();
