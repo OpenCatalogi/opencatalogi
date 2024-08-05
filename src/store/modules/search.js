b86146551da87704b288b9b2cc000dd699d09b43
Mwest2020 <mwesterweel@hotmail.com>
2024-08-05 14:08:39 +0200

    linting in modules stores

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -2,45 +2,45 @@
 import { defineStore } from 'pinia'
 
 export const useSearchStore = defineStore(
-    'search', {
-        state: () => ({
-            search: '',
-            searchResults: '',
-        }),
-    actions: {
-        setSearch(search) {
-            this.search = search
-            console.log('Active search set to ' + search)
-        },
-        setSearchResults(searchResults) {
-            this.searchResults = searchResults
-            console.log('Active search set to ' + searchResults)
-        },
-        getSearchResults() {
-            fetch(
-                '/index.php/apps/opencatalogi/api/search?_search=' + this.search,
-                {
-                    method: 'GET',
-                },
-            )
-                .then(
-                    (response) => {
-                    response.json().then(
-                            (data) => {
-                            this.searchResults = data
-                            }
-                        )
-                    }
-                )
-                .catch(
-                    (err) => {
-                    console.error(err)
-                    }
-                )
-        },
-        clearSearch() {
-            this.search = ''
-        },
-        },
-    }
+	'search', {
+		state: () => ({
+			search: '',
+			searchResults: '',
+		}),
+		actions: {
+			setSearch(search) {
+				this.search = search
+				console.log('Active search set to ' + search)
+			},
+			setSearchResults(searchResults) {
+				this.searchResults = searchResults
+				console.log('Active search set to ' + searchResults)
+			},
+			getSearchResults() {
+				fetch(
+					'/index.php/apps/opencatalogi/api/search?_search=' + this.search,
+					{
+						method: 'GET',
+					},
+				)
+					.then(
+						(response) => {
+							response.json().then(
+								(data) => {
+									this.searchResults = data
+								},
+							)
+						},
+					)
+					.catch(
+						(err) => {
+							console.error(err)
+						},
+					)
+			},
+			clearSearch() {
+				this.search = ''
+			},
+		},
+	},
 )
