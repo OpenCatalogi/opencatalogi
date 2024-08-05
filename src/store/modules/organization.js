2404957a10c25d19d1844c7ce216fb492ca8874b
Ruben van der Linde <ruben@conduction.nl>
2024-08-05 23:34:59 +0200

    Finishing the stores

--- src/store/modules/organization.js
+++ src/store/modules/organization.js
@@ -8,102 +8,35 @@ export const useOrganizationStore = defineStore('organization', {
 		organizationList: [],
 	}),
 	actions: {
-		setPublicationItem(publicationItem) {
-		// To prevent forms etc from braking we alway use a default/skeleton object
-			this.publicationItem = publicationItem && new Publication(publicationItem)
-			console.log('Active publication item set to ' + publicationItem && publicationItem.id)
+		setCOrganisationItem(organizationItem) {
+			this.organizationItem = organizationItem && new Organisation(organizationItem)
+			console.log('Active theme item set to ' + organizationItem && organizationItem?.id)
 		},
-		setPublicationList(publicationList) {
-			this.publicationList = publicationList.map((publicationItem) => new Publication(publicationItem))
-			console.log('Active publication item set to ' + publicationList.length)
+		setOrganisationList(organizationList) {
+			this.organizationList = organizationList.map(
+				(organizationItem) => new Organisation(organizationItem),
+			)
+			console.log('Organisation list set to ' + organizationList.length + ' items')
 		},
-		async refreshPublicationList(search = null) {
+		async refreshOrganisationList(search = null) {
 			// @todo this might belong in a service?
-			let endpoint = '/index.php/apps/opencatalogi/api/publications'
+			let endpoint = '/index.php/apps/opencatalogi/api/organisations'
 			if (search !== null && search !== '') {
 				endpoint = endpoint + '?_search=' + search
 			}
-			return fetch(
-				endpoint,
-				{
-					method: 'GET',
-				},
-			)
+			return fetch(endpoint, {
+				method: 'GET',
+			})
 				.then((response) => {
 					response.json().then((data) => {
-						this.setPublicationList(data?.results)
-						return data
-					})
-				})
-				.catch((err) => {
-					console.error(err)
-					return err
-				})
-		},
-		getPublicationAttachments(publication) { // @todo this might belong in a service?
-			fetch(
-				'/index.php/apps/opencatalogi/api/attachments',
-				{
-					method: 'GET',
-				},
-			)
-				.then((response) => {
-					response.json().then((data) => {
-						this.publicationAttachments = data.results.map(
-							(attachmentItem) => new Attachment(attachmentItem),
+						this.organizationList = data.results.map(
+							(organizationItem) => new Organisation(organizationItem),
 						)
-						return data
 					})
 				})
 				.catch((err) => {
 					console.error(err)
-					return err
 				})
-		},
-		getConceptPublications() { // @todo this might belong in a service?
-			fetch(
-				'/index.php/apps/opencatalogi/api/publications?status=concept',
-				{
-					method: 'GET',
-				},
-			)
-				.then((response) => {
-					response.json().then((data) => {
-						this.conceptPublications = data
-						return data
-					})
-				})
-				.catch((err) => {
-					console.error(err)
-					return err
-				})
-		},
-		getConceptAttachments() { // @todo this might belong in a service?
-			fetch(
-				'/index.php/apps/opencatalogi/api/attachments?status=concept',
-				{
-					method: 'GET',
-				},
-			)
-				.then((response) => {
-					response.json().then((data) => {
-						this.conceptAttachments = data
-						return data
-					})
-				})
-				.catch((err) => {
-					console.error(err)
-					return err
-				})
-		},
-		// @todo why does the following run through the store? -- because its impossible with props, and its vital information for the modal.
-		setPublicationDataKey(publicationDataKey) {
-			this.publicationDataKey = publicationDataKey
-			console.log('Active publication data key set to ' + publicationDataKey)
-		},
-		setAttachmentItem(attachmentItem) {
-			this.attachmentItem = attachmentItem && new Attachment(attachmentItem)
-			console.log('Active attachment item set to ' + attachmentItem)
 		},
 	},
 })
--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -8,102 +8,35 @@ export const useThemeStore = defineStore('theme', {
 		themeList: [],
 	}),
 	actions: {
-		setPublicationItem(publicationItem) {
-		// To prevent forms etc from braking we alway use a default/skeleton object
-			this.publicationItem = publicationItem && new Publication(publicationItem)
-			console.log('Active publication item set to ' + publicationItem && publicationItem.id)
+		setThemeItem(themeItem) {
+			this.themeItem = themeItem && new Theme(themeItem)
+			console.log('Active theme item set to ' + themeItem && themeItem?.id)
 		},
-		setPublicationList(publicationList) {
-			this.publicationList = publicationList.map((publicationItem) => new Publication(publicationItem))
-			console.log('Active publication item set to ' + publicationList.length)
+		setThemeList(themeList) {
+			this.themeList = themeList.map(
+				(themeItem) => new Theme(themeItem),
+			)
+			console.log('Theme list set to ' + themeList.length + ' items')
 		},
-		async refreshPublicationList(search = null) {
+		async refreshThemeList(search = null) {
 			// @todo this might belong in a service?
-			let endpoint = '/index.php/apps/opencatalogi/api/publications'
+			let endpoint = '/index.php/apps/opencatalogi/api/themes'
 			if (search !== null && search !== '') {
 				endpoint = endpoint + '?_search=' + search
 			}
-			return fetch(
-				endpoint,
-				{
-					method: 'GET',
-				},
-			)
+			return fetch(endpoint, {
+				method: 'GET',
+			})
 				.then((response) => {
 					response.json().then((data) => {
-						this.setPublicationList(data?.results)
-						return data
-					})
-				})
-				.catch((err) => {
-					console.error(err)
-					return err
-				})
-		},
-		getPublicationAttachments(publication) { // @todo this might belong in a service?
-			fetch(
-				'/index.php/apps/opencatalogi/api/attachments',
-				{
-					method: 'GET',
-				},
-			)
-				.then((response) => {
-					response.json().then((data) => {
-						this.publicationAttachments = data.results.map(
-							(attachmentItem) => new Attachment(attachmentItem),
+						this.themeList = data.results.map(
+							(themeItem) => new Theme(themeItem),
 						)
-						return data
 					})
 				})
 				.catch((err) => {
 					console.error(err)
-					return err
 				})
-		},
-		getConceptPublications() { // @todo this might belong in a service?
-			fetch(
-				'/index.php/apps/opencatalogi/api/publications?status=concept',
-				{
-					method: 'GET',
-				},
-			)
-				.then((response) => {
-					response.json().then((data) => {
-						this.conceptPublications = data
-						return data
-					})
-				})
-				.catch((err) => {
-					console.error(err)
-					return err
-				})
-		},
-		getConceptAttachments() { // @todo this might belong in a service?
-			fetch(
-				'/index.php/apps/opencatalogi/api/attachments?status=concept',
-				{
-					method: 'GET',
-				},
-			)
-				.then((response) => {
-					response.json().then((data) => {
-						this.conceptAttachments = data
-						return data
-					})
-				})
-				.catch((err) => {
-					console.error(err)
-					return err
-				})
-		},
-		// @todo why does the following run through the store? -- because its impossible with props, and its vital information for the modal.
-		setPublicationDataKey(publicationDataKey) {
-			this.publicationDataKey = publicationDataKey
-			console.log('Active publication data key set to ' + publicationDataKey)
-		},
-		setAttachmentItem(attachmentItem) {
-			this.attachmentItem = attachmentItem && new Attachment(attachmentItem)
-			console.log('Active attachment item set to ' + attachmentItem)
 		},
 	},
 })
