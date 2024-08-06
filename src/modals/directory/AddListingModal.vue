18219e74620cce441c27849d408a25facce7dac8
Barry Brands <barrybrands02@hotmail.com>
2024-08-06 14:37:39 +0200

    validate fix

--- src/modals/directory/AddListingModal.vue
+++ src/modals/directory/AddListingModal.vue
@@ -65,15 +65,7 @@ export default {
 			success: null,
 			error: false,
 			validateUrlError: null,
-			urlPattern: new RegExp(
-				'^(https?:\\/\\/)' // protocol
-                + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' // domain name
-                + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
-                + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
-                + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
-                + '(\\#[-a-z\\d_]*)?$',
-				'i',
-			), // fragment locator
+			urlPattern: /^(https?:\/\/)?(([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}$/i,
 		}
 	},
 	computed: {
