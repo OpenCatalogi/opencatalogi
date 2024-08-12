d9054a1672d0344e64ff50a4fc177663b8a6ae38
Thijn <Thijn@conduction.nl>
2024-08-12 14:43:11 +0200

    small fix

--- src/sidebars/search/SearchSideBar.vue
+++ src/sidebars/search/SearchSideBar.vue
@@ -23,7 +23,7 @@ import { searchStore, directoryStore, metadataStore } from '../../store/store.js
 			<template #icon>
 				<DatabaseOutline :size="20" />
 			</template>
-			<NcCheckboxRadioSwitch v-for="(listing, i) in directoryStore.listingList" type="switch">
+			<NcCheckboxRadioSwitch v-for="(listing, i) in directoryStore.listingList" :key="i" type="switch">
 				{{ listing.title || 'Geen titel' }}
 			</NcCheckboxRadioSwitch>
 		</NcAppSidebarTab>
@@ -31,7 +31,7 @@ import { searchStore, directoryStore, metadataStore } from '../../store/store.js
 			<template #icon>
 				<FileTreeOutline :size="20" />
 			</template>
-			<NcCheckboxRadioSwitch v-for="(metaData, i) in metadataStore.metaDataList" type="switch">
+			<NcCheckboxRadioSwitch v-for="(metaData, i) in metadataStore.metaDataList" :key="i" type="switch">
 				{{ metaData.title || 'Geen titel' }}
 			</NcCheckboxRadioSwitch>
 		</NcAppSidebarTab>
