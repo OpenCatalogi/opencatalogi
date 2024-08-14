8487bcdc95b9efc1d812447b5841693e4093e34d
Thijn <thijn@conduction.nl>
2024-08-14 15:11:31 +0200

    fixed some merge conflicts that gitkraken forgot about

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -1,56 +1,65 @@
 /* eslint-disable no-console */
 import { defineStore } from 'pinia'
 
-export const useSearchStore = defineStore(
-	'search', {
-		state: () => ({
-			search: '',
-			searchResults: '',
-			searchError: '',
-		}),
-		actions: {
-			setSearch(search) {
-				this.search = search
-				console.log('Active search set to ' + search)
-			},
-			setSearchResults(searchResults) {
-				this.searchResults = searchResults
-				console.log('Active search set to ' + searchResults)
-			},
-			/* istanbul ignore next */ // ignore this for Jest until moved into a service
-			getSearchResults() {
-				fetch(
-					'/index.php/apps/opencatalogi/api/search?_search=' + this.search,
-					{
-						method: 'GET',
+export const useSearchStore = defineStore('search', {
+	state: () => ({
+		search: '',
+		metadata: {},
+		searchResults: '',
+		searchError: '',
+	}),
+	actions: {
+		setSearch(search) {
+			this.search = search
+			console.log('Active search set to ' + search)
+		},
+		toggleMetadataSearch(metadataId, value = undefined) {
+			this.metadata[metadataId] = value || !this.metadata[metadataId] || false
+		},
+		setSearchResults(searchResults) {
+			this.searchResults = searchResults
+			console.log('Active search set to ' + searchResults)
+		},
+		/* istanbul ignore next */ // ignore this for Jest until moved into a service
+		getSearchResults() {
+			const enabledMetadataIds = Object.entries(this.metadata)
+				.filter(([key, value]) => value === true)
+				.map((metadata) => metadata[0])
+
+			console.log(enabledMetadataIds)
+
+			fetch(
+				'/index.php/apps/opencatalogi/api/search?_search=' + this.search,
+				{
+					method: 'GET',
+				},
+			)
+				.then(
+					(response) => {
+						response.json().then(
+							(data) => {
+								if (data?.code === 403 && data?.message) {
+									this.searchError = data.message
+									console.log(this.searchError)
+								} else {
+									this.searchError = '' // Clear any previous errors
+								}
+								this.searchResults = data
+							},
+						)
 					},
 				)
-					.then(
-						(response) => {
-							response.json().then(
-								(data) => {
-									if (data?.code === 403 && data?.message) {
-										this.searchError = data.message
-										console.log(this.searchError)
-									} else {
-										this.searchError = '' // Clear any previous errors
-									}
-									this.searchResults = data
-								},
-							)
-						},
-					)
-					.catch(
-						(err) => {
-							this.searchError = err.message || 'An error occurred'
-							console.error(err.message ?? err)
-						},
-					)
-			},
-			clearSearch() {
-				this.search = ''
-				this.searchError = ''
-			},
+				.catch(
+					(err) => {
+						this.searchError = err.message || 'An error occurred'
+						console.error(err.message ?? err)
+					},
+				)
+		},
+		clearSearch() {
+			this.search = ''
+			this.searchError = ''
 		},
 	},
+},
 )
