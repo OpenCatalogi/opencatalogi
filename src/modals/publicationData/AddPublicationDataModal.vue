5f7880d884f451336cdea83dc2b5f6fe8a28d44a
Thijn <thijn@conduction.nl>
2024-08-16 13:34:11 +0200

    added dropdown based on metadata properties

--- src/modals/publicationData/AddPublicationDataModal.vue
+++ src/modals/publicationData/AddPublicationDataModal.vue
@@ -20,21 +20,19 @@ import { navigationStore, publicationStore } from '../../store/store.js'
 				</NcNoteCard>
 			</div>
 			<div v-if="success === null" class="form-group">
-				<NcTextField :disabled="loading"
-					label="Naam"
-					required
-					:value.sync="key"
-					:loading="loading" />
+				<NcSelect v-bind="mapMetadataEigenschappen"
+					v-model="eigenschappen.value"
+					required />-
 
 				<NcTextField :disabled="loading"
 					label="Data"
-					:value.sync="value"
+					:value.sync="data"
 					:loading="loading" />
 			</div>
 
 			<span class="flex-horizontal">
 				<NcButton v-if="success === null"
-					:disabled="loading || !key || !value"
+					:disabled="loading || !eigenschappen.value?.id || !data"
 					type="primary"
 					@click="AddPublicatieEigenschap()">
 					<template #icon>
@@ -62,6 +60,7 @@ import {
 	NcTextField,
 	NcNoteCard,
 	NcLoadingIcon,
+	NcSelect,
 } from '@nextcloud/vue'
 
 // icons
@@ -72,24 +71,39 @@ export default {
 	components: {
 		NcModal,
 		NcTextField,
+		NcSelect,
 		NcButton,
 		NcNoteCard,
 		NcLoadingIcon,
 	},
 	data() {
 		return {
-			key: '',
-			value: '',
+			eigenschappen: {},
+			data: '',
 			loading: false,
 			success: null,
 			error: false,
 		}
 	},
+	computed: {
+		mapMetadataEigenschappen() {
+			return {
+				inputLabel: 'Publicatie type eigenschap',
+				options: Object.values(publicationStore.publicationItem?.metaData?.properties)
+					.filter((prop) => !Object.keys(publicationStore.publicationItem?.data).includes(prop.title))
+					.map((prop) => ({
+						id: prop.title,
+						label: prop.title,
+					})),
+			}
+		},
+	},
 	methods: {
 		AddPublicatieEigenschap() {
 			this.loading = true
+
 			const bodyData = publicationStore.publicationItem
-			bodyData.data[this.key] = this.value
+			bodyData.data[this.eigenschappen.value?.label] = this.data
 			delete bodyData.publicationDate
 
 			fetch(
@@ -124,8 +138,8 @@ export default {
 					}, 2000)
 
 					// reset modal form
-					this.key = ''
-					this.value = ''
+					this.eigenschappen = {}
+					this.data = ''
 				})
 				.catch((err) => {
 					this.loading = false
